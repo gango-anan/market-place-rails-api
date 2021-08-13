b8c80173ecfafa0cebe9458a6179cd713f0617d3
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-13 22:42:47 +0300

    Add products relationship to user#show

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,9 @@ class Api::V1::UsersController < ApplicationController
     # GET /users/1
     def show
         # render json: User.find(params[:id])
-        render json: UserSerializer.new(@user).serializable_hash
+        # render json: UserSerializer.new(@user).serializable_hash
+        options = { include: [:products] }
+        render json: UserSerializer.new(@user, options).serializable_hash
     end
 
     # POST /users
